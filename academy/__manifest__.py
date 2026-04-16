b48b6588f240a00e8395b2db174d205330f5076d
MuniishMaanka <odoo@eupd20.odoo.com>
2026-04-16 06:29:39 +0000

    [ADD] Demo course Demo

--- academy/__manifest__.py
+++ academy/__manifest__.py
@@ -12,6 +12,8 @@
     'category': 'Custom Modules/Tech Training',
     'depends': ['base'],
     'data': [],
-    'demo': [],
+    'demo': [
+        'demo/course_demo.xml',
+    ],
     'application': True,
 }
