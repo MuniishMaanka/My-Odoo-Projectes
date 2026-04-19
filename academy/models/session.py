b3218b3a22f7ee4e3a645ae6a362674b1195bee5
MuniishMaanka <odoo@eupd23.odoo.com>
2026-04-19 14:11:28 +0000

    ADD Session feature last validation

--- academy/models/session.py
+++ academy/models/session.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class Session(models.Model):
     _name = 'academy.session'
@@ -7,5 +8,23 @@ class Session(models.Model):
     name = fields.Char(string="Title")
     session_number = fields.Char(
         string="Session Number",
-        default=lambda self: self.env['ir.sequence'].next_by_code('session.number')
+        default="S0000",
+        copy=False,
+        required=True,
+        readonly=True
     )
+    date_start = fields.Datetime(string='Start Date', required=True)
+    date_end = fields.Datetime(string='End Date', required=True)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get('session_number', 'S0000') == 'S0000':
+                vals['session_number'] = self.env['ir.sequence'].next_by_code('session.number')
+        return super().create(vals_list)
+
+    @api.constrains('date_start', 'date_end')
+    def _check_end_date(self):
+        for session in self:
+            if session.date_start and session.date_end and session.date_start > session.date_end:
+                raise ValidationError('The end date cannot be before the start date.')
