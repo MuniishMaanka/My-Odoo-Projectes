2b19804116ece1e9cd1475a8a5b2f051ef2ae3f1
MuniishMaanka <odoo@eupd23.odoo.com>
2026-04-19 13:33:44 +0000

    ADD Session feature

---
+++ academy/models/session.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models
+
+class Session(models.Model):
+    _name = 'academy.session'
+    _description = 'Session Info'
+
+    name = fields.Char(string="Title")
+    session_number = fields.Char(
+        string="Session Number",
+        default=lambda self: self.env['ir.sequence'].next_by_code('session.number')
+    )
