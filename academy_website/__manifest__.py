8c181c3ef4f0adaba31672b42896a2d2041bb2ea
MuniishMaanka <odoo@eupd21.odoo.com>
2026-04-21 14:12:12 +0000

    fix for routes

--- academy_website/__manifest__.py
+++ academy_website/__manifest__.py
@@ -1,23 +1,13 @@
 {
     'name': "academy_website",
-
     'author': "Odoo, Inc",
     'website': "https://www.odoo.com",
-
-    'license': 'LGPL-3',
-
     'category': 'Custom Modules/Tech Training',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['academy', 'sale_management'],
-
-    # always loaded
+    'license': 'LGPL-3',
+    'depends': ['academy', 'website'],
     'data': [
         'views/academy_web_templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
     ],
     'auto_install': True,
 }
