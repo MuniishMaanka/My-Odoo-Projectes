686b0cee79c235d957f90a97623e14f16f8ce29b
Eng Muna Mustaf Isxaq <munamustafisaq@gmail.com>
2026-04-22 08:41:12 +0300

    fix: Controller

--- academy_website/controllers/academy_controllers.py
+++ academy_website/controllers/academy_controllers.py
@@ -11,6 +11,6 @@ class Academy(http.Controller):
         # Fetch all courses from the database
         courses = http.request.env['academy.course'].search([])
         # Render the template and pass the 'courses' variable to it
-        return http.request.render('academy.course_website', {
+        return http.request.render('academy_website.course_website', {
             'courses': courses,
         })
